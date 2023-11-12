e1e37520073bdb499000a38249140cfe55f2ed9d
josseline534 <josselinesanchezb@gmail.com>
2023-11-12 12:12:45 -0500

    fix [middleware]: update middleware error

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -2,20 +2,30 @@ const ERROR_TAG = '[ERROR-HANDLER]'
 
 // eslint-disable-next-line max-params
 module.exports.ErrorHandler = (error, { method, originalUrl }, res, _) => {
-  if ('status' in error) {
+  if (error.isBoom) {
     console.error(ERROR_TAG, {
       method,
       originalUrl,
-      status: error.status,
-      message: error.message
+      status: error.output.statusCode,
+      message: error.output.payload.message
     })
-    res.status(error.status).json(error)
+    res.status(error.output.statusCode).json(error.output.payload)
   } else {
-    console.error(ERROR_TAG, { method, originalUrl }, error.message)
-    res.status(500).json(error)
+    if ('status' in error) {
+      console.error(ERROR_TAG, {
+        method,
+        originalUrl,
+        status: error.status,
+        message: error.message
+      })
+      res.status(error.status).json(error)
+    } else {
+      console.error(ERROR_TAG, { method, originalUrl }, error.message)
+      res.status(500).json(error)
+    }
   }
 }
 
 module.exports.NotFoundErrorHandler = (_req, res) => {
-  res.status(400).json({ message: 'not found' })
+  res.status(404).json({ message: 'not found' })
 }
